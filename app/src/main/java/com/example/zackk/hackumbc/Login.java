a1cefc3649e703cf2e1ff4cdb0e05f303fdc6bcc
Travis Ho <thdarkblue@hotmail.com>
2016-10-01 13:30:43 -0400

    Push new create account activity

--- app/src/main/java/com/example/zackk/hackumbc/Login.java
+++ app/src/main/java/com/example/zackk/hackumbc/Login.java
@@ -2,13 +2,31 @@ package com.example.zackk.hackumbc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
+
+    private Button createAccountButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        System.out.println("Did this commit to master branch");
+        createMenu();
+    }
+
+    protected void createMenu() {
+        createAccountButton = (Button) findViewById(R.id.create_account_button);
+        createAccountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("Button Clicked");
+                Intent i = new Intent(Login.this, CreateAccountActivity.class);
+                startActivity(i);
+            }
+        });
+
     }
 }
