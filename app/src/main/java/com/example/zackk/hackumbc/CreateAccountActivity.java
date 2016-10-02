7e66e0ee00aad762b624202a953d568552e58a82
Zachariyya Khan <zack.khan101@gmail.com>
2016-10-02 03:30:37 -0400

    firebase stuff 3 (Login Authentication)

--- app/src/main/java/com/example/zackk/hackumbc/CreateAccountActivity.java
+++ app/src/main/java/com/example/zackk/hackumbc/CreateAccountActivity.java
@@ -76,12 +76,14 @@ public class CreateAccountActivity extends AppCompatActivity {
                         public void onComplete(@NonNull Task<AuthResult> task) {
                             Toast.makeText(CreateAccountActivity.this, "Logged In Success", Toast.LENGTH_SHORT).show();
                            fb = new Firebase("https://hackersearch-5723c.firebaseio.com/");
-                          Firebase fbchild = fb.child("user");
+                          Firebase fbchild = fb.child("email");
+                            Firebase fbchild2 = fb.child("username");
+                            fbchild2.setValue(usernameText.getText().toString());
                             fbchild.setValue(emailText.getText().toString());
                             Intent login = new Intent(CreateAccountActivity.this, Login.class);
                             startActivity(login);
                             if(!task.isSuccessful()) {
-                                Toast.makeText(CreateAccountActivity.this, "Logged In GitFucked", Toast.LENGTH_SHORT).show();
+                                Toast.makeText(CreateAccountActivity.this, "Logged In Failed", Toast.LENGTH_SHORT).show();
 
                             }
                         }
